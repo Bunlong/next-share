ae222f2dc8b149cc41fadb0a009d068cc784e4d6
Bunlong <bunlong.van@gmail.com>
2022-06-26 15:33:45 +0700

    Add HatenaShareCount

--- src/components/counts/FacebookShareCount.ts
+++ src/components/counts/FacebookShareCount.ts
@@ -2,7 +2,10 @@ import jsonp from 'jsonp';
 
 import createShareCount from '../../hocs/createShareCount';
 
-function getFacebookShareCount(shareUrl: string, callback: (shareCount?: number) => void) {
+function getFacebookShareCount(
+  shareUrl: string,
+  callback: (shareCount?: number) => void,
+) {
   const endpoint = `https://graph.facebook.com/?id=${shareUrl}&fields=og_object{engagement}`;
 
   jsonp(endpoint, (err: any, data: any) => {
---
+++ src/components/counts/HatenaShareCount.ts
@@ -0,0 +1,23 @@
+import jsonp from 'jsonp';
+
+import objectToGetParams from '../../utils';
+import createShareCount from '../../hocs/createShareCount';
+
+function getHatenaShareCount(
+  shareUrl: string,
+  callback: (shareCount?: number) => void,
+) {
+  const url = 'https://bookmark.hatenaapis.com/count/entry';
+
+  jsonp(
+    url +
+      objectToGetParams({
+        url: shareUrl,
+      }),
+    (err, data) => {
+      callback(data ? data : undefined);
+    },
+  );
+}
+
+export default createShareCount(getHatenaShareCount);
